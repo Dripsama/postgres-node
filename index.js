b80a461f9d7236aba945d415650f02e9c3347e33
Nikhil Saju <nikhilfacebook9@gmail.com>
2021-06-01 22:57:09 +0530

    port changed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const pool = require("./db");
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //=> req.body
 
@@ -73,6 +74,6 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Listening on port 5000");
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
